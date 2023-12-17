587edd0b0b7a93eeec54eb35fd162e1d1dfac864
lostbean <talktoedgar@gmail.com>
2023-12-17 10:31:50 -0300

    init

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule CvmExplorer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :cvm_explorer,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 2.2"},
+      {:iconv, "~> 1.0"},
+      {:zstream, "~> 0.6.4"}
+    ]
+  end
+end
---
+++ test/cvm_explorer_test.exs
@@ -0,0 +1,8 @@
+defmodule CvmExplorerTest do
+  use ExUnit.Case
+  doctest CvmExplorer
+
+  test "greets the world" do
+    assert CvmExplorer.hello() == :world
+  end
+end
